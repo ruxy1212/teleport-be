86d2b3714cf830c75cd641d8b3c9a46b3e299aa2
Russell Oje <russelloje@gmail.com>
2024-07-08 12:42:15 +0100

    Tah tah tah

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::prefix('auth')->middleware(['api'])->group(function(){
-//     Route::post('register', [AuthController::class, 'register'])->name('api.register');
-//     Route::post('login', [AuthController::class, 'login'])->name('api.login');    
-// });
+Route::prefix('auth')->middleware(['api'])->group(function(){
+    Route::post('register', [AuthController::class, 'register'])->name('api.register');
+    Route::post('login', [AuthController::class, 'login'])->name('api.login');    
+});
